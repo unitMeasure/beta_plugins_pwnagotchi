9a1d7e6f16b469864f960f5abe1233ca2e85e916
Avi <avipars@users.noreply.github.com>
2025-04-14 10:40:32 -0700

    minor fixes

--- pwnStart.py
+++ pwnStart.py
@@ -4,13 +4,13 @@ from pwnagotchi.utils import StatusFile
 
 class pwnStart(plugins.Plugin):
     __author__ = 'avipars'
-    __version__ = '0.0.1'
+    __version__ = '0.0.2'
     __license__ = 'GPL3'
     __name__ = 'pwnStart'
     __description__ = 'A plugin to disable auto_tune and fix_services, then enable probenpwn extensions'
     __github__ = 'https://github.com/avipars'
     __defaults__ = {
-        "enabled": False,
+        "enabled": False
     }
     def __init__(self):
         self.running = False
@@ -24,37 +24,37 @@ class pwnStart(plugins.Plugin):
         # Update status file
         self.status.update(data={'enabled': True})
         
-        # Disable auto_tune
-        if hasattr(self.options, 'auto_tune') and self.options['auto_tune']:
-            logging.info("[pwnStart] Disabling auto_tune")
-            self.options['auto_tune'] = False
-        else:
-            logging.info("[pwnStart] auto_tune was already disabled or not found")
+        try: 
+            # Disable auto_tune
+            if hasattr(self.options, 'auto_tune') and self.options['auto_tune']:
+                logging.info("[pwnStart] Disabling auto_tune")
+                self.options['auto_tune'] = False
+            else:
+                logging.info("[pwnStart] auto_tune was already disabled or not found")
+                
+            # Disable fix_services
+            if hasattr(self.options, 'fix_services') and self.options['fix_services']:
+                logging.info("[pwnStart] Disabling fix_services")
+                self.options['fix_services'] = False
+            else:
+                logging.info("[pwnStart] fix_services was already disabled or not found")
             
-        # Disable fix_services
-        if hasattr(self.options, 'fix_services') and self.options['fix_services']:
-            logging.info("[pwnStart] Disabling fix_services")
-            self.options['fix_services'] = False
-        else:
-            logging.info("[pwnStart] fix_services was already disabled or not found")
+            # Disable fix_services
+            if hasattr(self.options, 'NoGPSPrivacy') and self.options['NoGPSPrivacy']:
+                logging.info("[pwnStart] Disabling NoGPSPrivacy")
+                self.options['NoGPSPrivacy'] = False
+            else:
+                logging.info("[pwnStart] NoGPSPrivacy was already disabled or not found")
             
-        # Enable probenpwn extensions
-        try:
-            logging.info("[pwnStart] Enabling probenpwn extensions")
-            # This could be modified based on how the probenpwn extensions need to be enabled
-            # Method 1: If probenpwn is a separate plugin
-            if 'probenpwn' in self.options:
-                self.options['probenpwn']['enabled'] = True
-                logging.info("[pwnStart] Enabled probenpwn via options")
-            
-            # Method 2: If probenpwn needs system configuration
-            import subprocess
-            subprocess.run(['systemctl', 'enable', 'probenpwn.service'], check=True)
-            subprocess.run(['systemctl', 'start', 'probenpwn.service'], check=True)
-            logging.info("[pwnStart] Enabled probenpwn via systemd")
-            
+            # Enable probenpwn extensions
+            if hasattr(self.options, 'probenpwn') and self.options['probenpwn']:
+                logging.info("[pwnStart] Enabling probenpwn")
+                self.options['probenpwn'] = True
+            else:
+                logging.info("[pwnStart] probenpwn was already disabled or not found")
+                
         except Exception as e:
-            logging.error(f"[pwnStart] Error enabling probenpwn extensions: {e}")
+            logging.error(f"[pwnStart] Error with extensions: {e}")
             
         logging.info("[pwnStart] Configuration complete!")
     
