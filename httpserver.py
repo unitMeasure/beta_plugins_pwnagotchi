8899c797e127d4a8b9e079b10cdf6a9a99a1ff82
Avi <avipars@users.noreply.github.com>
2025-04-14 08:52:45 -0700

    server

---
+++ httpserver.py
@@ -0,0 +1,54 @@
+import logging
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import pwnagotchi
+
+import pwnagotchi.plugins as plugins
+from pwnagotchi.ui.components import LabeledValue
+from pwnagotchi.ui.view import BLACK
+import pwnagotchi.ui.fonts as fonts
+
+DIRECTORY = pwnagotchi.config['main']['bettercap']['handshakes']
+
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self):
+        super().__init__(directory=DIRECTORY)
+
+class HttpServerPlugin(plugins.Plugin):
+    __author__ = 'Hades , edited by @avipars'
+    __version__ = '1.0.0.1'
+    __license__ = 'GPL3'
+    __description__ = 'HTTP Server Plugin'
+    __github__ = 'https://github.com/itsdarklikehell/pwnagotchi-plugins/blob/master/httpserver.py'
+    __defaults__ = {
+        "enabled": False,
+    }
+
+    def on_loaded(self):
+        logging.info("[HttpServerPlugin] Loaded")
+        self.start_http_server()
+
+    def on_unload(self, ui):
+        logging.info("[HttpServerPlugin] Unloaded")
+        self.stop_http_server()
+
+    def start_http_server(self):
+        try:
+            server_address = ('', 8000)
+            self.httpd = HTTPServer(server_address, MyHTTPRequestHandler)
+            logging.info("[HttpServerPlugin] Starting HTTP server on port 8000")
+            self.httpd.serve_forever()
+
+        except Exception as e:
+            logging.error(f"[HttpServerPlugin] Error starting HTTP server: {e}")
+
+    def stop_http_server(self):
+        try:
+            if hasattr(self, 'httpd'):
+                logging.error("[HttpServerPlugin] Shutting Down HTTP Server.")
+                self.httpd.shutdown()
+
+        except Exception as e:
+            logging.error(f"[HttpServerPlugin] Error stopping HTTP server: {e}")
+
+# Instantiate the plugin
+http_server_plugin = HttpServerPlugin()
