7837390ae84610f3c6cf40e9d8d9cb557dcc9810
Avi <avipars@users.noreply.github.com>
2025-10-23 13:16:46 -0400

    Update probeReq.py

--- probeReq.py
+++ probeReq.py
@@ -9,7 +9,7 @@ class probeReq(plugins.Plugin):
     __GitHub__ = "https://github.com/unitMeasure/pwn-plugins/"
     __author__ = "avipars"
     __editor__ = 'avipars'
-    __version__ = "0.0.1.1"
+    __version__ = "0.0.1.2"
     __license__ = "GPL3"
     __description__ = "Listens for Wi-Fi probe requests and displays them on screen"
     __name__ = "probeReq"
@@ -63,8 +63,8 @@ class probeReq(plugins.Plugin):
             stat += "rssi:%s" % probe["rssi"]
             vend = probe['vendor']
             if vend and len(vend) >= 1: # has a vendor
-               stat += "\nvend:%s" % vend
-            stat += "mac:%s" % probe['mac']
+               stat += "\n vend:%s" % vend
+            stat += "\n mac:%s" % probe['mac']
         
         self.pr_status = stat
         if 'logging' in self.options and self.options['logging']:
@@ -79,5 +79,3 @@ class probeReq(plugins.Plugin):
                     logging.info(f"[{self.__class__.__name__}] plugin unloaded")
             except Exception as e:
                 logging.error(f"[{self.__class__.__name__}] unload: %s" % e)
-
-
